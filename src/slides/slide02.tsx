ee7c2535e576adc71cc787c4ff5fcd5ffc53bd01
Thomas Sterren <thomas.sterren@gmail.com>
2020-01-23 09:30:14 +0100

    Added some demo slides

---
+++ src/slides/slide02.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SlideTitle from '../components/SlideTitle'
+import SlidePage from '../components/SlidePage'
+
+const Slide = () => (
+    <SlidePage>
+        <SlideTitle>How ticks the Internet</SlideTitle>
+        <br />
+        <div>
+            <p>Have you ever thought about how the Internet is ticking?</p>
+            <p>Is it the same time everywhere in the world, or is it different in every country?</p>
+        </div>
+    </SlidePage>
+)
+
+export default Slide;
