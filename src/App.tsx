300e4a19538cc6d71b77b25ef1846e449f463649
Thomas Sterren <thomas.sterren@gmail.com>
2020-01-23 16:04:57 +0100

    Added ability to change slides with left and right arrow keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { nextSlide, prevSlide } from './store/actions/slideActions'
 import './App.css';
 import Presentation from './components/Presentation'
+import { useKeyPress } from './hooks'
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();                           // Function to send action to redux store
+  const arrowRightPressed = useKeyPress('ArrowRight');      // Listen to 'arrow right' key pressed events
+  const arrowLeftPressed = useKeyPress('ArrowLeft');        // Listen to 'arrow left' key pressed events
+
+  // Handle keys to go to next slide
+  useEffect(() => {
+
+    // Trigger next slide on following keyboard events: 
+    // - ArrowRigth
+    (arrowRightPressed) && dispatch(nextSlide());
+    // eslint-disable-next-line
+  }, [arrowRightPressed])
+
+  // Handle keys to go to previous slide
+  useEffect(() => {
+
+    // Trigger previous slide on following keyboard events: 
+    // - ArrowLeft
+    (arrowLeftPressed) && dispatch(prevSlide());
+    // eslint-disable-next-line
+  }, [arrowLeftPressed])
+
   return (
     <div className="App">
       <Presentation />
--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setSlideCount } from '../store/actions/slideActions'
+import { useCurrentSlideSelector } from '../store/selectors/slides'
 import slides from '../slides'
 
 interface Props {
@@ -8,13 +10,21 @@ interface Props {
 
 const Presentation: React.FC<Props> = (props) => {
 
-    const CurrentSlide = slides[0];
+    const currentSlide = useCurrentSlideSelector();
+    const CurrentSlide = slides[currentSlide];
+    const dispatch = useDispatch();                         // Function to send action to redux store
+
+    // Initialize slides 'count' variable
+    useEffect(() => {
+        dispatch(setSlideCount(slides.length))
+        // eslint-disable-next-line
+    }, [])
 
     return (
-        <div>
+        <>
             {/* Show actual slide */}
             <CurrentSlide />
-        </div>
+        </>
     )
 }
 
