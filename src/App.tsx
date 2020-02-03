ae3df893e791dc579412788416593294df041cab
Thomas Sterren <thomas.sterren@gmail.com>
2020-02-03 20:02:53 +0100

    Added 'space', 'home' and 'end' keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
-import { nextSlide, prevSlide } from './store/actions/slideActions'
+import { nextSlide, prevSlide, firstSlide, lastSlide } from './store/actions/slideActions'
 import './App.css';
 import Presentation from './components/Presentation'
 import { useKeyPress } from './hooks'
@@ -9,15 +9,18 @@ const App: React.FC = () => {
   const dispatch = useDispatch();                           // Function to send action to redux store
   const arrowRightPressed = useKeyPress('ArrowRight');      // Listen to 'arrow right' key pressed events
   const arrowLeftPressed = useKeyPress('ArrowLeft');        // Listen to 'arrow left' key pressed events
+  const spacePressed = useKeyPress(' ');
+  const homePressed = useKeyPress('Home');
+  const endPressed = useKeyPress('End');
 
   // Handle keys to go to next slide
   useEffect(() => {
 
     // Trigger next slide on following keyboard events: 
     // - ArrowRigth
-    (arrowRightPressed) && dispatch(nextSlide());
+    (arrowRightPressed || spacePressed) && dispatch(nextSlide());
     // eslint-disable-next-line
-  }, [arrowRightPressed])
+  }, [arrowRightPressed, spacePressed])
 
   // Handle keys to go to previous slide
   useEffect(() => {
@@ -27,6 +30,19 @@ const App: React.FC = () => {
     (arrowLeftPressed) && dispatch(prevSlide());
     // eslint-disable-next-line
   }, [arrowLeftPressed])
+
+  // Go to first slide if 'home' button is pressed on keyboard
+  useEffect(() => {
+
+    homePressed && dispatch(firstSlide());
+    // eslint-disable-next-line
+  }, [homePressed])
+
+  // Go to last slide if 'end' button is pressed on keyboard
+  useEffect(() => {
+    endPressed && dispatch(lastSlide());
+    // eslint-disable-next-line
+  }, [endPressed])
 
   return (
     <div className="App">
