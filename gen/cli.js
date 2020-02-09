475ff705cee68f79e9c323e1cc0d8a615d71ee9a
Thomas Sterren <thomas.sterren@gmail.com>
2020-02-09 08:30:06 +0100

    Added representer executable

---
+++ gen/cli.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function gen() {
+    console.log('start generation...');
+    console.log('generation done');
+}
+exports.default = gen;
+gen();
---
+++ gen/cli.ts
@@ -0,0 +1,8 @@
+
+export default function gen() {
+    console.log('starting generation...');
+
+    console.log('generation done')
+}
+
+gen();
